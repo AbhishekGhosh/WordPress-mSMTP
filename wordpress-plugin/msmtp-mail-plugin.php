9c58f92d3834228d974f89651d7769fa4f79464f
Abhishek Ghosh <admin@thecustomizewindows.com>
2023-05-29 22:34:26 +0530

    Create msmtp-mail-plugin.php

---
+++ wordpress-plugin/msmtp-mail-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: msmtp mail plugin
+ * Plugin URI: https://thecustomizewindows.com
+ * Description: Helps WordPress to send email using msmtp configaration on server
+ * Author: Abhishek_Ghosh
+ * Author URI: https://thecustomizewindows.com
+ * Version: 1.0
+ */
+
+/**
+ * This function will connect wp_mail to your authenticated SMTP server.
+ * All SMTP_CONSTANTS are defined in wp-config.php
+ */
+add_action( 'phpmailer_init', 'send_smtp_email' );
+function send_smtp_email( $phpmailer ) {
+        if ( ! is_object( $phpmailer ) ) {
+                $phpmailer = (object) $phpmailer;
+        }
+
+        $phpmailer->Mailer     = 'smtp';
+        $phpmailer->Host       = SMTP_HOST;
+        $phpmailer->SMTPAuth   = SMTP_AUTH;
+        $phpmailer->Port       = SMTP_PORT;
+        $phpmailer->Username   = SMTP_USER;
+        $phpmailer->Password   = SMTP_PASS;
+        $phpmailer->SMTPSecure = SMTP_SECURE;
+        $phpmailer->From       = SMTP_FROM;
+        $phpmailer->FromName   = SMTP_NAME;
+}
