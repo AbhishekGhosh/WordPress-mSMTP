1e711cc2753a99562a583d24cac7113e5c7f7236
Abhishek Ghosh <admin@thecustomizewindows.com>
2023-05-29 22:42:48 +0530

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,13 @@
+// add these before the line which says
+/* That's all, stop editing! Happy blogging. */
+
+define( 'SMTP_HOST',   'smtp.office365.com' ); 
+define( 'SMTP_PORT',   '587' ); 
+define( 'SMTP_SECURE', 'tls' );
+define( 'SMTP_AUTH',    true ); 
+define( 'SMTP_USER',   'username@hotmail.com' );	
+define( 'SMTP_PASS',   'your-password' );
+define( 'SMTP_FROM',   'username@hotmail.com' );
+define( 'SMTP_NAME',   'microsoft' );	
+
+/** Absolute path to the WordPress directory. */
